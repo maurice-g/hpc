4f3814d56c78f9b0776a3661f5a8c806af640b6d
Stefannn <Stefan@hpc12.ch>
2012-12-07 16:05:33 +0100

    cublas version added

--- MatrixMultiplication/TestingRoutines/BlasSharedMemTest/dgemm_cublas.cu
+++ MatrixMultiplication/TestingRoutines/BlasSharedMemTest/dgemm_cublas.cu
@@ -7,41 +7,46 @@
 #include <matrix.hpp>
 #include <fillMatrix.hpp>
 #include <Timer.hpp>
-#include <chrono>
+#include <Measurement.hpp>
 
 	
-void
-dgemm_cublas(MatrixType &A, MatrixType &B, MatrixType & C) {
-		//assert A columnmajor
-        char transA = 'N';
-        int M = A.num_rows();
-        int N = B.num_cols();
-        int K = A.num_cols();
-        double alpha = 1.;
-        int LDA = A.leading_dimension();
-        int LDB = B.leading_dimension();
-        double beta = 0.;
-        int LDC = C.leading_dimension();
-        cublasDgemm(transA,transA,M,N,K,alpha,A.data(),LDA,B.data(),LDB,beta,C.data(),LDC);
-
-}
-
 
 int main() {
 
 	typedef hpc12::matrix<double,hpc12::column_major> matrix_type;
-	for (int N = 512;N < 20000;N*=2) {
+	for (int N = 512;N < 10000;N*=2) {
 		matrix_type A(N,N);
 		matrix_type B(N,N);
 		matrix_type C(N,N);
+		
+		double * d_A, * d_B, *d_C;
+		cudaMalloc((void**) &d_A, N*N*sizeof(double));
+                cudaMalloc((void**) &d_B, N*N*sizeof(double));
+                cudaMalloc((void**) &d_C, N*N*sizeof(double));
+
 		fillMatrix(A);
 		fillMatrix(B);
-		//C = A*B
+
 		Timer _t(1);
-		dgemm_libsci(A,B,C);
+
+		cudaMemcpy (d_A, A.data(),N*N*sizeof(double),cudaMemcpyHostToDevice);
+		cudaMemcpy (d_B, B.data(),N*N*sizeof(double),cudaMemcpyHostToDevice);
+		cudaMemset (d_C, 0., N*N*sizeof(double));
+		//C = A*B
+		char trans = 'N';
+		double alpha = 1.;
+		double beta = 0.;
+		cublasDgemm(trans,trans,N,N,N,alpha,d_A,N,d_B,N,beta,d_C,N);
+//dgemm_cublas(d_A,d_B,d_C);
+		cudaMemcpy(C.data(),d_C,N*N*sizeof(double),cudaMemcpyDeviceToHost);
+		
 		_t.stop();
-		Measurement m("dgemm with libsci,?noacc?,16thrds",N,N,_t.elapsed_s());
+		Measurement m("cublasDgemm",N,N,_t.elapsed_s());
 		std::cout << m;
+		//free memory on device
+		cudaFree(d_A);
+		cudaFree(d_B);
+		cudaFree(d_B);
 	}
 
 
