9ec5620e187056665a2616487f0b24daeab5c01d
StefanH <Stefan@hpc12.ch>
2012-12-14 15:49:20 +0100

    Constructor

--- MatrixMultiplication/DistGEMM/DistGEMM.cpp
+++ MatrixMultiplication/DistGEMM/DistGEMM.cpp
@@ -2,13 +2,48 @@
 #include <MPI.h>
 #include <libsci_acc.h>
 #include <cassert>
+#include <cmath>
 
-
-DistGEMM::DistGEMM(int N, int P) {
+DistGEMM::DistGEMM(int n, int numprocs, int cubes) {
+	std::assert(cubes*cubes*cubes == numprocs);	
+	std::assert(n % cubes == 0);
 	
+	MPI_Comm_size(MPI_COMM_WORLD,&size);
+	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+
+	P = numprocs;
+	N = n;
+	cubeSize = cubes;					//# processors in each direction of cartesian topology
 
 	//build MPI topology
-	int nums[3] = 
+	int nums[3];						//number of nodes in each dimension (x,y,z)
+	MPI_Dims_create(size,3,nums);
+	
+	//build global communicator, then subdivide into comm_i, comm_j, comm_k
+	int periodic[3] = {0, 0, 0};
+	MPI_Comm cart_comm;
+	MPI_Cart_create(MPI_COMM_WORLD,3,nums,periodic,true,&cart_comm);
+	
+	//assign comm_i, comm_j, comm_k
+	int logv_i = {true,false,false};
+	int logv_j = {false,true,false};
+	int logv_k = {false,false,true};
+	MPI_Cart_sub(cart_comm,logv_i,comm_i);
+	MPI_Cart_sub(cart_comm,logv_j,comm_j);
+	MPI_Cart_sub(cart_comm,logv_k,comm_k);
+	
+	//assign the coordinates to each rank
+	int d = 3;
+	int coords[3];
+	MPI_Cart_coords(cart_comm,rank,d,coords);
+	p_i = coords[0];
+	p_j = coords[1];
+	p_k = coords[2];
+	
+	libsci_acc_HostAlloc((void**)&A, sizeof(val_type)*N*N);
+	libsci_acc_HostAlloc((void**)&B, sizeof(val_type)*N*N);
+	libsci_acc_HostAlloc((void**)&B, sizeof(val_type)*N*N);
+	
 	
 }
 
