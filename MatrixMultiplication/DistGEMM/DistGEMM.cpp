0dd944e480177a290e8152d3c8776561fa13b4da
Maurice <m.gonzenbach@briskvision.com>
2012-12-14 15:41:16 +0100

    added Makefile, main.cpp modified DistGEMM.cpp

--- MatrixMultiplication/DistGEMM/DistGEMM.cpp
+++ MatrixMultiplication/DistGEMM/DistGEMM.cpp
@@ -21,4 +21,12 @@ void DistGEMM::performGEMM() {
 	MPI_Bcast(A, blocksize*blocksize, mpi_val_type, root_k, comm_k);
 	// send root_i's Matrix B to all other processors in communicator comm_i
 	MPI_Bcast(B, blocksize*blocksize, mpi_val_type, root_i, comm_i);
+
+	char transA = 'N';
+	double alpha = 1.;
+	double beta = 0.;
+
+	dgemm(transA, transA, blocksize, blocksize, blocksize, alpha, A, N, B, N, beta, C, N);
+
+	MPI_Reduce(C, C, blocksize*blocksize, mpi_val_type, MPI_SUM, root_j, comm_j);
 }	
---
+++ MatrixMultiplication/DistGEMM/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "distGEMM.hpp"
+
+int main(int argc, char* argv[]) {
+	distGEMM m(600, 20);
+
+	return 0;
+}
