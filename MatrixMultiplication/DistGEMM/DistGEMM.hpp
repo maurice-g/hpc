e834d33f1915bfb9a2abaf83120cb9be5e49898e
StefanH <Stefan@hpc12.ch>
2013-01-04 11:14:16 +0100

    added doxygen comments in DistGEMM.hpp

--- MatrixMultiplication/DistGEMM/DistGEMM.hpp
+++ MatrixMultiplication/DistGEMM/DistGEMM.hpp
@@ -1,3 +1,4 @@
+
 #ifndef MATRIXMULT
 #define MATRIXMULT
 
@@ -5,7 +6,15 @@
 #include <string>
 
 /*
+* \class DistGEMM
 * \brief Class implementing distributed gemm for a n x n matrix
+*/
+/**
+* This class implements a distributed (MPI) gemm using the 3d-matrix-multiplicaiton
+* algorithm described in....
+* there are several possibilities for setting up the matrices:
+* \li load matrices from file, \sa setup
+* \li -generate matrices on each node individually (lehmer matrices)
 */
 
 
@@ -16,17 +25,50 @@ public:
 	#define mpi_val_type	MPI_DOUBLE
 
 	/*!
-	* Constructor
-	* @param N is the size of the entire matrix
-	* @param P is the number of nodes 
+	* Constructor, ! Requirements: N mod d == 0, d^3 == P
+	* @param N is the size of the matrix
+	* @param P is the number of nodes
+	* @param d is the size of the 3-dimensional node-topology
+	* 
 	*/
-
 	DistGEMM(int N, int P, int d);					// constructor: initialize communicators & topology, matrix allocation
+	
+	/*! 
+	* Destructor: Frees Memory of matrices A,B and C
+	*/
 	~DistGEMM();
+	
+	
+	/*!
+	* testing function \n
+	* each node fills its (local) matrix part in a way
+	* such that the global matrices are lehmer matrices
+	*/ 
 	void initializeLehmer();					// initialize Lehmer matrix A (where k=0) and B (where i=0)
+	
+	
+	
+	/*!
+	* This function performs C = A*B
+	* using the distributed 3d-matrix-multiplication algorithm
+	* the local computation on each node are performed using the 
+	* libsci_acc version of dgemm (which distributes work between gpu-cpu)
+	*/
 	void performGEMM();						// perform the matrix multiplication
-	void output_result(std::string filename);			
-	void readMatrix(std::string filename, val_type *matrix);
+	
+	/*!
+	*This function writes the matrix C (the result of A*B) to a file using the following format:\n
+	* Column major storage, but as a single array (column)
+	*@param filename
+	**/
+	void output_result(std::string filename);
+	
+		
+	/*!
+	* Use this function to load the matrices A,B from files (filenameA,filenameB)\n
+	* Required Format: plain-text files, NxN entries in one column\n
+	* these numbers are interpreted as the matrix entries stored in column major format
+	**/
 	void setup(std::string filenameA, std::string filenameB);						
 
 private:
@@ -40,6 +82,8 @@ private:
 	count_type cubeSize;					//MPI 3d Topology length ->(P^1/3)
 	int root_i, root_j, root_k;
 
+	//function gets called from setup()
+	void readMatrix(std::string filename, val_type *matrix);
 };
 
 
